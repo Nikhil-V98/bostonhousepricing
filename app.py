994e86ba254f503d91966639ca96cb5dcfcb2ce2
Nikhil <91378299+Nikhil-V98@users.noreply.github.com>
2022-09-03 11:28:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-heroku buildpacks:set heroku/python
 
 app = Flask(__name__)
 ## Load the model
