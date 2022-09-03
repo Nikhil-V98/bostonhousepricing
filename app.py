595edfd761cc074f20dcbf299277cfc35a649d4b
Nikhil <91378299+Nikhil-V98@users.noreply.github.com>
2022-09-03 11:26:35 +0530

    Update app.py
    with heroku buid pack

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
+$ heroku buildpacks:set heroku/python
 app = Flask(__name__)
 ## Load the model
 regmodel = pickle.load(open('regmodel.pkl','rb'))
